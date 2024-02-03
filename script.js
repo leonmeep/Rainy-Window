5407856329d8e03ca517f54d81485382f246b117
Leonbirch <leonbirch333@gmail.com>
2024-02-03 14:12:47 +0000

    added event listener to images and flexboxed them

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const changeText = document.querySelector('.drip');
+
+changeText.addEventListener("mouseover", function() {
+  changeText.textContent = "drip, drop";
+
+}) 
+
+
+const changetext = document.querySelector('.drop');
+
+changetext.addEventListener("mouseover", function() {
+  changetext.textContent = "drip, drop";
+
+})
